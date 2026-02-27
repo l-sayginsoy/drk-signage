58c66532000a0fb6aec181f10ecb90dc30ee96c1
Levent <deineechte@mail.de>
2026-02-28 00:29:51 +0100

    Add assets to public for /assets paths

--- config.ts
+++ config.ts
@@ -1,4 +1,1 @@
-// WICHTIG: Ersetzen Sie den Platzhalter unten durch Ihren echten API-Schlüssel von OpenWeatherMap.
-// Sie können einen kostenlosen Schlüssel hier erhalten: https://openweathermap.org/appid
-
 export const OPENWEATHER_API_KEY = import.meta.env.VITE_OPENWEATHER_API_KEY || "";
